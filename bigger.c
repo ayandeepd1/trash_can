6295e0b0a66b531ba3330c80c19c0f39775434a9
ayandeepd1 <89993737+ayandeepd1@users.noreply.github.com>
2022-03-04 00:06:13 +0530

    Add files via upload

--- bigger.c
+++ bigger.c
@@ -1,9 +1,12 @@
 #include<stdio.h>
 int main(){
     printf("enter 2 numbers with space in between: ");
+    
     float a, b;
+    
     scanf("%f %f", &a, &b);
     float max= a>b?a:b;
     printf("\n%0.1f is bigger",max);
+    (a>b)?printf("\na is bigger"):printf("\nb is bigger");
     return 0;
 }
