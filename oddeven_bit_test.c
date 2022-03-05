bb302dab5a4a75a2d06f1353e319b2b25a527d36
ayandeepd1 <89993737+ayandeepd1@users.noreply.github.com>
2022-03-06 04:44:41 +0530

    Update and rename oddeven.c to oddeven_bit_test.c

--- oddeven_bit_test.c
+++ oddeven_bit_test.c
@@ -3,8 +3,7 @@ int main(){
     printf("enter a number: ");
     int a;
     scanf("%d", &a);
-    a&=1;
-    (a==0)?printf("\neven number"):printf("\nodd number");
+    ((a&1)==0)?printf("\neven number"):printf("\nodd number");
     //(a>b)?printf("\na is bigger"):printf("\nb is bigger");
 
     return 0;
